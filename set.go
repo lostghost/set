d11097eb190119e42eba8b9155cff65dfc9c3eda
Ken <ken@bradsdeals.com>
2013-05-19 22:32:05 -0500

    Adding additional methods. Adding tests. Updating docs.

--- set.go
+++ set.go
@@ -1,18 +1,16 @@
+// The set package implements a set collection -- a store a unique values without order.
 package set
 
-import "sync"
-
-type Set struct {
-	sync.RWMutex
-	m map[interface{}]bool
-}
+type Set map[interface{}]bool
 
 type MapFunction func(interface{}) interface{}
 
-// creates a new, initially empty set structure.
+type FilterFunction func(interface{}) bool
+
+// Creates a new set structure. The New constructor can take a variadic paramater to initialize the set with an initial set of values.
 func New(items ...interface{}) Set {
 	var set Set
-	set.m = make(map[interface{}]bool)
+	set = make(map[interface{}]bool)
 	if len(items) > 0 {
 		for i := range items {
 			set.Add(items[i])
@@ -21,48 +19,37 @@ func New(items ...interface{}) Set {
 	return set
 }
 
-// adds the element item to the set, if it is not present already.
+// Adds the element item to the set, if it is not present already.
 func (s Set) Add(item interface{}) {
-	s.Lock()
-	s.m[item] = true
-	s.Unlock()
+	s[item] = true
 }
 
-// removes the element item from the set, if it is present.
+// Removes the element item from the set, if it is present.
 func (s Set) Remove(item interface{}) {
-	s.Lock()
-	delete(s.m, item)
-	s.Unlock()
+	delete(s, item)
 }
 
-// checks whether the value item is in the set.
+// Checks whether the value item is in the set.
 func (s Set) Contains(item interface{}) bool {
-	s.RLock()
-	_, exists := s.m[item]
-	s.RUnlock()
-	if exists {
-		return true
-	}
-	return false
+	_, exists := s[item]
+	return exists
 }
 
-// returns the number of elements in the set.
+// Returns the number of elements in the set.
 func (s Set) Size() int {
-	return len(s.m)
+	return len(s)
 }
 
-// returns a list containing the elements of the set in some arbitrary order.
+// Returns a list containing the elements of the set in some arbitrary order.
 func (s Set) Enumerate() []interface{} {
 	out := []interface{}{}
-	s.RLock()
-	for i := range s.m {
+	for i := range s {
 		out = append(out, i)
 	}
-	s.RUnlock()
 	return out
 }
 
-// checks whether the set is empty.
+// Checks whether the set is empty.
 func (s Set) IsEmpty() bool {
 	if s.Size() > 0 {
 		return false
@@ -70,34 +57,97 @@ func (s Set) IsEmpty() bool {
 	return true
 }
 
-// delete all elements of the set.
+// Delete all elements of the set.
 func (s Set) Clear() {
-	s.Lock()
-	for i := range s.m {
-		delete(s.m, i)
+	for i := range s {
+		delete(s, i)
 	}
-	s.Unlock()
 }
 
-// returns the set of distinct values resulting from applying function f to each element of the set.
+// Returns the set of distinct values resulting from applying function f to each element of the set.
 func (s Set) Map(f MapFunction) Set {
-	out := New()
-	s.RLock()
-	for i := range s.m {
-		out.Add(f(i))
+	o := New()
+	for i := range s {
+		o.Add(f(i))
 	}
-	s.RUnlock()
-	return out
+	return o
 }
 
-// TODO: union(S,T): returns the union of sets S and T.
-// TODO: intersection(S,T): returns the intersection of sets S and T.
-// TODO: difference(S,T): returns the difference of sets S and T.
-// TODO: subset(S,T): a predicate that tests whether the set S is a subset of set T.
+// Returns the subset containing all elements of the set that satisfy a given predicate f.
+func (s Set) Filter(f FilterFunction) Set {
+	o := New()
+	for i := range s {
+		if f(i) {
+			o.Add(i)
+		}
+	}
+	return o
+}
+
+// Returns an arbitrary element of the set, deleting it from the set
+func (s Set) Pop() interface{} {
+	var item interface{}
+	for item = range s {
+		break
+	}
+	s.Remove(item)
+	return item
+}
+
+// A predicate that tests whether the set is a subset of set t.
+func (s Set) Subset(t Set) bool {
+	e := s.Enumerate()
+	o := true
+	for i := range e {
+		if t.Contains(e[i]) == false {
+			o = false
+			break
+		}
+	}
+	return o
+}
+
+// Returns the union of sets s and t.
+func Union(s, t Set) Set {
+	o := New(s.Enumerate()...)
+	e := t.Enumerate()
+	for i := range e {
+		o.Add(e[i])
+	}
+	return o
+}
+
+// Returns the intersection of sets s and t.
+func Intersection(s, t Set) Set {
+	o := New()
+	e := t.Enumerate()
+	for i := range e {
+		if s.Contains(e[i]) {
+			o.Add(e[i])
+		}
+	}
+	return o
+}
+
+// Returns the difference of sets s and t.
+func Difference(s, t Set) Set {
+	o := New()
+	e := t.Enumerate()
+	for i := range e {
+		if s.Contains(e[i]) == false {
+			o.Add(e[i])
+		}
+	}
+	e = s.Enumerate()
+	for i := range e {
+		if t.Contains(e[i]) == false {
+			o.Add(e[i])
+		}
+	}
+	return o
+}
+
 // TODO: iterate(S): returns a function that returns one more value of S at each call, in some arbitrary order.
-// TODO: build(x1,x2,…,xn,): creates a set structure with values x1,x2,…,xn.
 // TODO: create_with_capacity(n): creates a new set structure, initially empty but capable of holding up to n elements.
 // TODO: capacity(S): returns the maximum number of values that S can hold.
-// TODO: pop(S): returns an arbitrary element of S, deleting it from S.
-// TODO: filter(P,S): returns the subset containing all elements of S that satisfy a given predicate P.
 // TODO: equal(S1, S2): checks whether the two given sets are equal (i.e. contain all and only the same elements).
--- set_test.go
+++ set_test.go
@@ -2,6 +2,7 @@ package set
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
 
@@ -214,4 +215,260 @@ func TestMap(t *testing.T) {
 			t.Errorf("The result of the int * 2 mapping function should contain the value %d", items[i]*2)
 		}
 	}
+}
+
+func TestFilter(t *testing.T) {
+	testSet := New("ken", "brad", "jeff", "ryan", "tim", "greg", "scott")
+
+	filterFunc := func(i interface{}) bool {
+		if len(i.(string)) == 4 {
+			return true
+		}
+		return false
+	}
+
+	includedItems := []interface{}{"brad", "jeff", "ryan", "greg"}
+	excludedItems := []interface{}{"ken", "tim", "scott"}
+
+	filterSet := testSet.Filter(filterFunc)
+	if filterSet.Size() != len(includedItems) {
+		t.Errorf("There should be %d elements in the filtered set (%d found)", len(includedItems), filterSet.Size())
+	}
+
+	for i := range includedItems {
+		if filterSet.Contains(includedItems[i]) == false {
+			t.Errorf("All four letter items should be inluded in the filtered set (%v missing)", includedItems[i])
+		}
+	}
+	for i := range excludedItems {
+		if filterSet.Contains(excludedItems[i]) {
+			t.Errorf("Only four letter items should be included in the filtered set (%v unexpected)", excludedItems[i])
+		}
+	}
+}
+
+func TestPop(t *testing.T) {
+	items := []interface{}{"english", "spanish", "french", "italian"}
+	testSet := New(items...)
+	setSize := testSet.Size()
+	item := testSet.Pop()
+
+	if testSet.Size() != setSize-1 {
+		t.Errorf("Pop on a non-zero size set should reduce the set size by one")
+	}
+
+	if testSet.Contains(item) {
+		t.Errorf("The item returned by Pop should no longer be contained in the set")
+	}
+
+	found := false
+	for i := range items {
+		if item == items[i] {
+			found = true
+			break
+		}
+	}
+	if found == false {
+		t.Errorf("The result of Pop should have been one of the items that was added to the set")
+	}
+
+	testSet = New()
+	item = testSet.Pop()
+	if item != nil {
+		t.Errorf("The expected result of Pop on an empty set is nil, the actual result was %v", item)
+	}
+
+	for i := 0; i < 100; i++ {
+		testSet.Add(i)
+	}
+	for i := 0; i < 100; i++ {
+		_ = testSet.Pop()
+	}
+	if testSet.IsEmpty() == false {
+		t.Errorf("Popping the same number of unique items that were added should result in an empty set")
+	}
+}
+
+func TestSubset(t *testing.T) {
+	testSet := New("one", "two", "three", 4, 5, 6)
+	superSet := New("one", "two", "three", "four", "five", "six", 1, 2, 3, 4, 5, 6)
+	altSet := New(1, 2, 3, 4, 5, 6)
+
+	if testSet.Subset(superSet) == false {
+		t.Errorf("testSet should be a subset of superSet")
+	}
+
+	if testSet.Subset(altSet) {
+		t.Errorf("testSet is not a subset of altSet")
+	}
+}
+
+func TestUnion(t *testing.T) {
+	groupOne := []interface{}{"red", "blue", "green"}
+	groupTwo := []interface{}{"red", "yellow", "purple", "black"}
+
+	setOne := New(groupOne...)
+	setTwo := New(groupTwo...)
+	setOneSize := setOne.Size()
+	setTwoSize := setTwo.Size()
+
+	unionSet := Union(setOne, setOne)
+	if unionSet.Size() != setOne.Size() {
+		t.Errorf("The size of the union of two identical sets should be the size of the original set")
+	}
+
+	unionSet = Union(setOne, setTwo)
+	if setOne.Size() != setOneSize {
+		t.Errorf("Union should not affect the size of the first set")
+	}
+	if setTwo.Size() != setTwoSize {
+		t.Errorf("Union should not affect the size of the second set")
+	}
+
+	if unionSet.Size() != (setOne.Size() + setTwo.Size() - 1) {
+		t.Errorf("The size of the union of two sets with one common item should be the sum of the sizes of the two sets minus 1")
+	}
+
+	for i := range groupOne {
+		if unionSet.Contains(groupOne[i]) == false {
+			t.Errorf("The union of two sets should contain all of the elements of the first set, missing %v", groupOne[i])
+		}
+	}
+	for i := range groupTwo {
+		if unionSet.Contains(groupTwo[i]) == false {
+			t.Errorf("The union of two sets should contain all of the elements of the second set, missing %v", groupTwo[i])
+		}
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	groupOne := []interface{}{"red", "blue", "green"}
+	groupTwo := []interface{}{"red", "yellow", "purple", "black", "blue"}
+
+	groupIntersection := []interface{}{"red", "blue"}
+	groupDifference := []interface{}{"green", "yellow", "purple", "black"}
+
+	setOne := New(groupOne...)
+	setTwo := New(groupTwo...)
+	setOneSize := setOne.Size()
+	setTwoSize := setTwo.Size()
+
+	intersectionSet := Intersection(setOne, setOne)
+	if intersectionSet.Size() != setOne.Size() {
+		t.Errorf("The size of the intersection of two identical sets should be the size of the original set")
+	}
+
+	intersectionSet = Intersection(setOne, setTwo)
+	if setOne.Size() != setOneSize {
+		t.Errorf("Intersection should not affect the size of the first set")
+	}
+	if setTwo.Size() != setTwoSize {
+		t.Errorf("Intersection should not affect the size of the second set")
+	}
+
+	if intersectionSet.Size() != len(groupIntersection) {
+		t.Errorf("The size of the intersection of two sets with %d common items should be %d", len(groupIntersection), len(groupIntersection))
+	}
+
+	for i := range groupIntersection {
+		if intersectionSet.Contains(groupIntersection[i] == false) {
+			t.Errorf("The intersection should contain all of the common items: missing %v", groupIntersection[i])
+		}
+	}
+
+	for i := range groupDifference {
+		if intersectionSet.Contains(groupDifference[i]) {
+			t.Errorf("The intersection should not contain any of the items that are not common to both sets: not expecting %v", groupDifference[i])
+		}
+	}
+}
+
+func TestDifference(t *testing.T) {
+	groupOne := []interface{}{"red", "blue", "green"}
+	groupTwo := []interface{}{"red", "yellow", "purple", "black", "blue"}
+
+	groupIntersection := []interface{}{"red", "blue"}
+	groupDifference := []interface{}{"green", "yellow", "purple", "black"}
+
+	setOne := New(groupOne...)
+	setTwo := New(groupTwo...)
+	setOneSize := setOne.Size()
+	setTwoSize := setTwo.Size()
+
+	differenceSet := Difference(setOne, setOne)
+	if differenceSet.Size() != 0 {
+		t.Errorf("The size of the difference of two identical sets should be 0")
+	}
+
+	differenceSet = Difference(setOne, setTwo)
+	if setOne.Size() != setOneSize {
+		t.Errorf("Difference should not affect the size of the first set")
+	}
+	if setTwo.Size() != setTwoSize {
+		t.Errorf("Difference should not affect the size of the second set")
+	}
+
+	if differenceSet.Size() != len(groupDifference) {
+		t.Errorf("The size of the difference of two sets with %d diffent items should be %d", len(groupDifference), len(groupDifference))
+	}
+
+	for i := range groupDifference {
+		if differenceSet.Contains(groupDifference[i] == false) {
+			t.Errorf("The difference should contain all of the different items: missing %v", groupDifference[i])
+		}
+	}
+
+	for i := range groupIntersection {
+		if differenceSet.Contains(groupIntersection[i]) {
+			t.Errorf("The difference should not contain any of the items that are common to both sets: not expecting %v", groupIntersection[i])
+		}
+	}
+}
+
+func ExampleSet() {
+	// Creating a new empty set
+	set := New()
+
+	// Add items to the set
+	set.Add("blue")
+	set.Add("red")
+	set.Add("green")
+
+	// How many items does the set contain?
+	log.Println(set.Size()) // 3
+
+	// Adding a duplicate item doesn't change the set size
+	set.Add("blue")
+	log.Println(set.Size()) // 3
+
+	// Test to see if a set contains a specific item
+	log.Println(set.Contains("blue")) // true
+
+	// Remove an item from the set
+	set.Remove("blue")
+	log.Println(set.Contains("blue")) // false
+
+	// Create a set with initial values
+	set = New(1, 2, 3, 5, 7, 11)
+
+	// Find the intersection of two sets
+	multiplesOf2 := New(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+	multiplesOf3 := New(3, 6, 9, 12, 15, 18, 21)
+	multiplesOf2And3 := Union(multiplesOf2, multiplesOf3)
+	log.Println(multiplesOf2And3.Enumerate()) // {6, 12, 18}
+}
+
+func ExampleNew() {
+	// Create an empty set
+	emptySet := New()
+	log.Println(emptySet.IsEmpty()) // true
+
+	// Create a set with initial values
+	colors := New("red", "blue", "green")
+	log.Println(colors.Size()) // 3
+
+	// Create a set with initial values from a slice of interfaces
+	slice := []interface{}{1, 2, 3, 4, "one", "two", "three", "four"}
+	numbers := New(slice...)
+	log.Println(numbers.Size()) // 8
 }
